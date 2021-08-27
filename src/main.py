0d55d1da73f920a4316ef844fb419fcd800cfae0
Shingo Hattori <shinnkun2222@gmail.com>
2021-08-27 22:12:47 +0900

    add fitsreader
    fitsデータを読み込んでcsvとして出力する機能を追加した．
    
    自作のfitsは配列の作成で間違いがあったため，実験するときはDSHARPのデータを直接引っ張ってきて実行するべし．

--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
 from functions import FitsHandler
+import sys
+import numpy as np
 
+def main(fits_path, csv_path):
+    handler = FitsHandler(fits_path)
+    handler.open_fits()
 
-handler = FitsHandler("../sample_datas/sample.fits")
-handler.open_fits()
-print(handler.header())
+    header = handler.header()
+    with open("{}_header.txt".format(csv_path), "w") as f:
+        f.write(str(header))
+
+    # DSHARPデータは(画像データ，枚数，縦，横)の配列になっているので画像のみ取り出す
+    counter = 0
+    datas = handler.data()[0]
+    for data in datas:
+        np.savetxt("{}_data_{}.csv".format(csv_path, counter), data)
+        counter += 1
+
+def usage():
+    print("Usage: main.py fits_path save_path")
+
+def help():
+    print("Usage: main.py fits_path save_path")
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) <= 2:
+        usage()
+    else:
+        fits_path = args[1]
+        csv_path = args[2]
+        main(fits_path, csv_path)
